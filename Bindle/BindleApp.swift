da97a894b2bb05a1d97d20ac521041a68bca796d
pravalgautam <pravalgautam79@gmail.com>
2023-09-10 12:25:46 +0530

    Initial Commit

---
+++ Bindle/BindleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BindleApp.swift
+//  Bindle
+//
+//  Created by Praval Gautam on 10/09/23.
+//
+
+import SwiftUI
+
+@main
+struct BindleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
